7cf7071a817283da798ba0f00c9cc27fcbca515e
Andriy_Yarish <andriyyarish@gmail.com>
2021-08-04 11:06:17 +0300

    initial commit

---
+++ line_detector.py
@@ -0,0 +1,99 @@
+import cv2
+import numpy as np
+from shapely.geometry import LineString
+
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    # channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def apply_mask(image):
+    """
+    Apply color selection to RGB images to blackout everything except for white and yellow lane lines.
+        Parameters:
+            image: An np.array compatible with plt.imshow.
+    """
+    # White color mask
+    lower_threshold = np.uint8([180, 180, 180])
+    # lower_threshold = np.uint8([230, 230, 230])
+    upper_threshold = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(image, lower_threshold, upper_threshold)
+
+    # Combine white and yellow masks
+    mask = white_mask  # cv2.bitwise_or(white_mask, yellow_mask)
+    masked_image = cv2.bitwise_and(image, image, mask=mask)
+
+    return masked_image
+
+
+def drow_the_lines(img, lines):
+    if lines is None:
+        return
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), (50, 100, 200), thickness=2)
+    cv2.line(img, (0, 300), (1500, 300), (100, 100, 200), thickness=2)
+    return img
+
+
+# image = cv2.imread('init.png')
+
+def detect_lines(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    height = image.shape[0]
+    width = image.shape[1]
+    region_of_interest_vertices = [
+        (700, 1080),
+        (950, 1080),
+        (1200, 100)
+    ]
+    # gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur_image = cv2.medianBlur(image, 5)
+    gray_image = apply_mask(blur_image)
+    # cv2.imshow("gray_image", gray_image)
+
+    canny_image = cv2.Canny(gray_image, 75, 150)
+    # cv2.imshow("canny_image", canny_image)
+    cropped_image = region_of_interest(canny_image,
+                                       np.array([region_of_interest_vertices], np.int32), )
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho=2,
+                            theta=np.pi / 180,
+                            threshold=50,
+                            lines=np.array([]),
+                            minLineLength=80,
+                            maxLineGap=60)
+    return lines
+
+
+def line_intersection_dection(bounding_line, line_target_line):
+    if line_target_line is None:
+        return
+    if bounding_line is None:
+        return
+    # bounding_line = (([0, 0, 1900, 1200],),)
+    result = {}
+    for key in line_target_line:
+        for line in bounding_line:
+            bounding_a = (line[0][0], line[0][1])
+            bounding_b = (line[0][2], line[0][3])
+
+            line_to_check_a = line_target_line.get(key)[0]
+            line_to_check_b = line_target_line.get(key)[1]
+
+            line_bounding = LineString([bounding_a, bounding_b])
+            line_to_check = LineString([line_to_check_a, line_to_check_b])
+
+            intersection_point = line_bounding.intersection(line_to_check)
+
+            if not intersection_point.is_empty:
+                if intersection_point.xy[1][0] > 300:
+                    print(intersection_point)
+                    # return intersection_point.xy[0]
+                    result[key] = intersection_point
+    return result
--- object_tracker.py
+++ object_tracker.py
@@ -1,5 +1,7 @@
-from absl import flags
 import sys
+
+from absl import flags
+
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
@@ -7,11 +9,7 @@ import time
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
-import tensorflow as tf
-from yolov3_tf2.models import YoloV3
-from yolov3_tf2.dataset import transform_images
-from yolov3_tf2.utils import convert_boxes
+from _collections import deque
 
 from deep_sort import preprocessing
 from deep_sort import nn_matching
@@ -19,51 +17,116 @@ from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
 
+from line_detector import detect_lines, line_intersection_dection
+from line_detector import drow_the_lines
+
+import tensorflow as tf
+from yolov3_tf2.models import YoloV3
+from yolov3_tf2.dataset import transform_images
+from yolov3_tf2.utils import convert_boxes
+
 class_names = [c.strip() for c in open('./data/labels/coco.names').readlines()]
 yolo = YoloV3(classes=len(class_names))
 yolo.load_weights('./weights/yolov3.tf')
 
 max_cosine_distance = 0.5
 nn_budget = None
-nms_max_overlap = 0.8
+nms_max_overlap = 0.8 # ROIs that overlap more than this values are suppressed.
 
 model_filename = 'model_data/mars-small128.pb'
 encoder = gdet.create_box_encoder(model_filename, batch_size=1)
 metric = nn_matching.NearestNeighborDistanceMetric('cosine', max_cosine_distance, nn_budget)
 tracker = Tracker(metric)
 
-vid = cv2.VideoCapture('./data/video/car_2.mp4')
+bounding_lines = None
+
+vid = cv2.VideoCapture('./data/video/doroga1.mp4')
 
 codec = cv2.VideoWriter_fourcc(*'XVID')
-vid_fps =int(vid.get(cv2.CAP_PROP_FPS))
-vid_width,vid_height = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+vid_fps = int(vid.get(cv2.CAP_PROP_FPS))
+vid_width, vid_height = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter('./data/video/results.avi', codec, vid_fps, (vid_width, vid_height))
 
-from _collections import deque
-pts = [deque(maxlen=30) for _ in range(1000)]
+track_id_to_tracklet_map = {} ## tracklets
+
+
+def update_tracks_info(track, center):
+    if track_id_to_tracklet_map.get(track.track_id) is None:
+        track_id_to_tracklet_map[track.track_id] = deque(maxlen=40)
+    track_id_to_tracklet_map[track.track_id].append(center)
+    if not track.is_confirmed():
+        track_id_to_tracklet_map.pop(track.track_id, None)
+
+
+# retrieve start and end to be able to draw straight line for each track for further intersection detection
+def get_tracks_start_end_dots():
+    start_end_dict = {}
+    for key in track_id_to_tracklet_map:
+        start_end_dict[key] = [track_id_to_tracklet_map.get(key)[0],
+                               track_id_to_tracklet_map.get(key)[-1]]  # get first and last elements of the track dots
+    return start_end_dict
+
+
+def clean_up_outdated_tracklets():
+    active_track_ids = set()
+    for track in tracker.tracks:
+        active_track_ids.add(track.track_id)
+    for k in list(track_id_to_tracklet_map.keys()):
+        if k not in active_track_ids:
+            del track_id_to_tracklet_map[k]
+
+
+def draw_tracklets(img):
+    clean_up_outdated_tracklets()
+    start_end_dots = get_tracks_start_end_dots()
+    if start_end_dots is None:
+        return
+    for key in start_end_dots:
+        # thickness = int(np.sqrt(64 / float(key + 1)) * 2)
+        cv2.line(img, start_end_dots[key][0], start_end_dots[key][1], (0, 255, 0), 4)
+
 
 counter = []
+frameCounter = 0
 
 while True:
-    _, img = vid.read()
+    _, img = vid.read()  # read frame by frame
     if img is None:
         print('Completed')
         break
 
-    img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_in = tf.expand_dims(img_in, 0)
-    img_in = transform_images(img_in, 416)
+    # if bounding_lines is None or frameCounter > 50:
+    #     # lines_image = cv2.imread('./data/video/initLines.png')
+    #     bounding_lines = detect_lines(img)
+    #     cv2.imwrite('./data/video/initLines.png', img)
+    #     time_line_initiated = time.time()
+    #     frameCounter = 0
+    # else:
+    #     frameCounter = frameCounter + 1
+    if bounding_lines is None:
+        # lines_image = cv2.imread('./data/video/initLines.png')
+        bounding_lines = detect_lines(img)
+        cv2.imwrite('./data/video/initLines.png', img)
+
+    img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert color from cv2 default format BGR to yolo RGB
+    img_in = tf.expand_dims(img_in, 0)  # 3d -> 4d array batch size | height | width | depth
+    img_in = transform_images(img_in, 416)  # resize to default yolo size
 
     t1 = time.time()
 
-    boxes, scores, classes, nums = yolo.predict(img_in)
+    boxes, scores, classes, nums = yolo.predict(img_in)  #
+    # boxes, 3d shape (1,100,4) - third param is x,y of the center width and height
+    # scores, 2d shape (1,100) - remaining filled with zeros
+    # classes, 2D shape (1, 100) - detected object class , remaining filled with zero
+    # nums, 1D shape (1) - total num of detected objects
 
     classes = classes[0]
     names = []
     for i in range(len(classes)):
         names.append(class_names[int(classes[i])])
     names = np.array(names)
-    converted_boxes = convert_boxes(img, boxes[0])
+    converted_boxes = convert_boxes(img, boxes[
+        0])  # due to image was resized box should be resized back according to original image size
     features = encoder(img, converted_boxes)
 
     detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in
@@ -79,52 +142,66 @@ while True:
     tracker.update(detections)
 
     cmap = plt.get_cmap('tab20b')
-    colors = [cmap(i)[:3] for i in np.linspace(0,1,20)]
+    colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
     current_count = int(0)
 
     for track in tracker.tracks:
-        if not track.is_confirmed() or track.time_since_update >1:
+        if not track.is_confirmed() or track.time_since_update > 1:
             continue
         bbox = track.to_tlbr()
-        class_name= track.get_class()
+        class_name = track.get_class()
         color = colors[int(track.track_id) % len(colors)]
         color = [i * 255 for i in color]
 
-        cv2.rectangle(img, (int(bbox[0]),int(bbox[1])), (int(bbox[2]),int(bbox[3])), color, 2)
-        cv2.rectangle(img, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)
-                    +len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-        cv2.putText(img, class_name+"-"+str(track.track_id), (int(bbox[0]), int(bbox[1]-10)), 0, 0.75,
+        # draw bounding box on the initial image
+        cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+        cv2.rectangle(img, (int(bbox[0]), int(bbox[1] - 30)), (int(bbox[0]) + (len(class_name)
+                                                                               + len(str(track.track_id))) * 17,
+                                                               int(bbox[1])), color, -1)
+        cv2.putText(img, class_name + "-" + str(track.track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75,
                     (255, 255, 255), 2)
 
-        center = (int(((bbox[0]) + (bbox[2]))/2), int(((bbox[1])+(bbox[3]))/2))
-        pts[track.track_id].append(center)
+        # calculate center of the bounding box to further use it for track drawing
+        center = (int(((bbox[0]) + (bbox[2])) / 2), int(((bbox[1]) + (bbox[3])) / 2))
+        update_tracks_info(track, center)
 
-        for j in range(1, len(pts[track.track_id])):
-            if pts[track.track_id][j-1] is None or pts[track.track_id][j] is None:
-                continue
-            thickness = int(np.sqrt(64/float(j+1))*2)
-            cv2.line(img, (pts[track.track_id][j-1]), (pts[track.track_id][j]), color, thickness)
+        # pts[track.track_id].append(center)
+        # for j in range(1, len(pts[track.track_id])):
+        #     if pts[track.track_id][j - 1] is None or pts[track.track_id][j] is None:
+        #         continue
+        #     thickness = int(np.sqrt(64 / float(j + 1)) * 2)
+        #     cv2.line(img, (pts[track.track_id][j - 1]), (pts[track.track_id][j]), color, thickness)
 
         height, width, _ = img.shape
-        cv2.line(img, (0, int(3*height/6+height/20)), (width, int(3*height/6+height/20)), (0, 255, 0), thickness=2)
-        cv2.line(img, (0, int(3*height/6-height/20)), (width, int(3*height/6-height/20)), (0, 255, 0), thickness=2)
+        # cv2.line(img, (0, int(3 * height / 6 + height / 20)), (width, int(3 * height / 6 + height / 20)), (0, 255, 0),
+        #          thickness=2)
+        # cv2.line(img, (0, int(3 * height / 6 - height / 20)), (width, int(3 * height / 6 - height / 20)), (0, 255, 0),
+        #          thickness=2)
 
-        center_y = int(((bbox[1])+(bbox[3]))/2)
+        center_y = int(((bbox[1]) + (bbox[3])) / 2)
 
-        if center_y <= int(3*height/6+height/20) and center_y >= int(3*height/6-height/20):
+        if center_y <= int(3 * height / 6 + height / 20) and center_y >= int(3 * height / 6 - height / 20):
             if class_name == 'car' or class_name == 'truck':
                 counter.append(int(track.track_id))
                 current_count += 1
 
+    draw_tracklets(img)
+    drow_the_lines(img, bounding_lines)
+    result = line_intersection_dection(bounding_lines, get_tracks_start_end_dots())
+
+    if result is not None or result.__len__() != 0:
+        for key in result:
+            print("Intersection detected for object with id {}", key)
+
     total_count = len(set(counter))
     cv2.putText(img, "Current Vehicle Count: " + str(current_count), (0, 80), 0, 1, (0, 0, 255), 2)
-    cv2.putText(img, "Total Vehicle Count: " + str(total_count), (0,130), 0, 1, (0,0,255), 2)
+    cv2.putText(img, "Total Vehicle Count: " + str(total_count), (0, 130), 0, 1, (0, 0, 255), 2)
 
-    fps = 1./(time.time()-t1)
-    cv2.putText(img, "FPS: {:.2f}".format(fps), (0,30), 0, 1, (0,0,255), 2)
-    cv2.resizeWindow('output', 1024, 768)
+    fps = 1. / (time.time() - t1)
+    cv2.putText(img, "FPS: {:.2f}".format(fps), (0, 30), 0, 1, (0, 0, 255), 2)
     cv2.imshow('output', img)
+    # cv2.resizeWindow('output', 1024, 768)
     out.write(img)
 
     if cv2.waitKey(1) == ord('q'):
